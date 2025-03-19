c6fc85cafaba81a9b18647298004a00d8076aabf
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-03-19 19:56:01 -0300

    structuring

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,22 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config} */
-export default {
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs,ts}"]
+export default [
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // Variáveis não utilizadas geram aviso
+      "semi": ["error", "always"], // Exige ponto e vírgula
+      "@typescript-eslint/no-explicit-any": "error", // Proíbe uso de `any`
+      "prefer-const": "warn" // Sugere `const` quando possível
     }
-  ],
-  languageOptions: {
-    globals: globals.browser
-  },
-  extends: [
-    "eslint:recommended",
-    ...tseslint.configs.recommended
-  ],
-  plugins: ["@typescript-eslint"],
-};
+  }
+];
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,9 @@
 console.log("index.ts")
+
+
+
+
+
+
+
+
